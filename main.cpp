239c01699da5efd5ec9aeb547fb894cb31904fb8
Артем <mobi82123@gmail.com>
2026-03-11 18:07:46 +0200

    Update main to test Visitors copy/move and operators

--- main.cpp
+++ main.cpp
@@ -1,26 +1,37 @@
+#include <iostream>
+#include <utility>
 #include "Visitors.h"
 #include "Animals.h"
 #include "Staff.h"
 
-int main() {
+using namespace std;
 
+int main() {
     Visitors v1;
     Visitors v2("Victor", 69, 81);
 
     Animals a1;
-    Animals a2("Pathera leo", 12, 150);
+    Animals a2("Panthera leo", 12, 150);
 
-    Staff s1;
-    Staff s2("Vasya", "cleaner", 69);
+    Staff st1;
+    Staff st2("Vasya", "cleaner", 69);
+
+    Visitors v3 = v2;
+    Visitors v4 = move(v3);
+    Visitors v5 = v1 + v2;
+
+    cout << Visitors::getCount() << endl;
 
     v1.display();
     v2.display();
+    v4.display();
+    v5.display();
 
     a1.display();
     a2.display();
 
-    s1.display();
-    s2.display();
+    st1.display();
+    st2.display();
 
     return 0;
 }
