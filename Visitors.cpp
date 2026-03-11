64c9a4e71e9167747a6bb4525dae46fcef05714f
Артем <mobi82123@gmail.com>
2026-03-11 18:07:16 +0200

    Implement operator overloading for Visitors class

--- Visitors.cpp
+++ Visitors.cpp
@@ -1,19 +1,67 @@
 #include "Visitors.h"
 #include <iostream>
+
 using namespace std;
 
+int Visitors::visitorCount = 0;
 
-Visitors::Visitors() : Visitors("Victor", 69, 81) {}
+Visitors::Visitors() {
+    name = "None";
+    age = 0;
+    ticket = 0;
+    visitorCount++;
+}
 
-Visitors::Visitors(string name , int age, int ticket) 
-	: name(name), age(age), ticket(ticket) {}
+Visitors::Visitors(string name, int age, int ticket) {
+    this->name = name;
+    this->age = age;
+    this->ticket = ticket;
+    visitorCount++;
+}
+
+Visitors::Visitors(const Visitors& other) {
+    name = other.name;
+    age = other.age;
+    ticket = other.ticket;
+    visitorCount++;
+}
+
+Visitors::Visitors(Visitors&& other) {
+    name = move(other.name);
+    age = other.age;
+    ticket = other.ticket;
+}
 
 Visitors::~Visitors() {
-	cout << "Visitors destroyed\n";
+    visitorCount--;
 }
-void Visitors::display() const {
-	cout << "Name: " << name
-		<< ", Age: " << age
-		<< ", Ticket: " << ticket << endl;
 
+void Visitors::display() const {
+    cout << "Name: " << name << ", Age: " << age << ", Ticket: " << ticket << endl;
+}
+
+void Visitors::setAge(int age) {
+    this->age = age;
+}
+
+int Visitors::getCount() {
+    return visitorCount;
+}
+
+Visitors Visitors::operator+(const Visitors& other) {
+    Visitors temp;
+    temp.name = this->name + " + " + other.name;
+    temp.age = this->age + other.age;
+    temp.ticket = this->ticket + other.ticket;
+    return temp;
+}
+
+ostream& operator<<(ostream& os, const Visitors& v) {
+    os << v.name << " " << v.age << " " << v.ticket;
+    return os;
+}
+
+istream& operator>>(istream& is, Visitors& v) {
+    is >> v.name >> v.age >> v.ticket;
+    return is;
 }
